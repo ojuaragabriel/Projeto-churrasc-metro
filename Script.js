011d28a86e5156e8b9618a0c2c5f9f8fba3253d7
Gαвriєℓ αrαυjσ <100657976+ojuaragabriel@users.noreply.github.com>
2022-03-27 22:50:01 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 // Carne - 400gr por pessoa + de 6 horas - 650
 // Cerveja - 1200 ml por pessoa + 6 horas - 2000 ml
 // Refrigerante/água - 1000 ml por pessoa + 6 horas 1500ml
+
 // Crianças valem por 0,5
 
 
